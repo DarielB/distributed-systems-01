31e4f069a8580d7d922b8e67954c73ba035e2f74
DarielB <darielbezerra13@gmail.com>
2025-06-04 11:29:52 -0300

    fixing identation

--- Socket/JavaServerSocket_Calculadora/CalculadoraServerSocket.java
+++ Socket/JavaServerSocket_Calculadora/CalculadoraServerSocket.java
@@ -13,49 +13,48 @@ public class CalculadoraServerSocket {
 	    Calculadora calc = new Calculadora();
 		try {
 			welcomeSocket = new ServerSocket(9090);
-		  int i=0; //numero de clientes
+			int i=0; //numero de clientes
 	  
-	      System.out.println ("Servidor no ar");
-	      while(true) { 
-	           Socket connectionSocket = welcomeSocket.accept(); 
-	           i++;
-	           System.out.println ("Nova conexao");
+	    	System.out.println ("Servidor no ar");
+	    	while(true) { 
+	        	Socket connectionSocket = welcomeSocket.accept(); 
+	        	i++;
+	        	System.out.println ("Nova conexao");
 
-	           // Interpretando dados do servidor
-	           socketEntrada = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-               String operacao= socketEntrada.readLine();
-               String oper1=socketEntrada.readLine();
-               String oper2=socketEntrada.readLine();
+	        	// Interpretando dados do servidor
+	        	socketEntrada = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            	String operacao= socketEntrada.readLine();
+            	String oper1=socketEntrada.readLine();
+            	String oper2=socketEntrada.readLine();
 
-               // Chamando a calculadora
-			 	String result= "";
-			   switch (operacao) {
-				case "+":
-					result += calc.soma(Double.parseDouble(oper1),Double.parseDouble(oper2)); 	
+            	// Chamando a calculadora
+				String result= "";
+				switch (operacao) {
+					case "+":
+						result += calc.soma(Double.parseDouble(oper1),Double.parseDouble(oper2)); 	
 					break;
-				case "-":
-					result += calc.subtracao(Double.parseDouble(oper1),Double.parseDouble(oper2)); 	
+					case "-":
+						result += calc.subtracao(Double.parseDouble(oper1),Double.parseDouble(oper2)); 	
 					break;
-				case "/":
-					result += calc.divisao(Double.parseDouble(oper1),Double.parseDouble(oper2)); 	
+					case "/":
+						result += calc.divisao(Double.parseDouble(oper1),Double.parseDouble(oper2)); 	
 					break;
-				case "*":
-					result += calc.multiplicacao(Double.parseDouble(oper1),Double.parseDouble(oper2)); 	
+					case "*":
+						result += calc.multiplicacao(Double.parseDouble(oper1),Double.parseDouble(oper2)); 	
 					break;					
-				default:
+					default:
 					break;
 			   }
 
-               //Enviando dados para o servidor
-               socketOutput= new DataOutputStream(connectionSocket.getOutputStream());
-	           socketOutput.writeBytes(result+ '\n');
-	           System.out.println (result);  
-	           socketOutput.flush();
-	           socketOutput.close();
+            	//Enviando dados para o servidor
+            	socketOutput= new DataOutputStream(connectionSocket.getOutputStream());
+	        	socketOutput.writeBytes(result+ '\n');
+	        	System.out.println (result);  
+	        	socketOutput.flush();
+	        	socketOutput.close();
 	      }
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 }
